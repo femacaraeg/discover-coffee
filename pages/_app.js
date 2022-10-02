14ec74e04169acc385ed9308511744dd9d7fd8d6
Fe Macaraeg <femacaraeg@gmail.com>
2022-10-02 21:20:35 +0800

    Code cleanup/

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import '../styles/globals.css';
 
 import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <React.Fragment>
       <Component {...pageProps} />
       <Footer />
-    </>
+    </React.Fragment>
   );
 }
 
