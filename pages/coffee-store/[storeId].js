8835a1b5d5988e7bd6da19126fedfa25d80ee154
Fe Macaraeg <femacaraeg@gmail.com>
2022-10-13 10:28:36 +0800

    Update render of dynamic page.

--- pages/coffee-store/[storeId].js
+++ pages/coffee-store/[storeId].js
@@ -3,25 +3,112 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Head from 'next/head';
+import Image from 'next/image';
 
-function CoffeeStore() {
+import coffeeStoresData from '../../mocks/coffee-stores.json';
+
+import styles from '../../styles/coffee-store.module.css';
+
+export function getStaticPaths() {
+  const paths = coffeeStoresData.map((store) => {
+    return {
+      params: {
+        storeId: store.id.toString(),
+      },
+    };
+  });
+  return {
+    fallback: true,
+    paths,
+  };
+}
+
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((store) => {
+        return store.id.toString() === params.storeId;
+      }),
+    },
+  };
+}
+
+function CoffeeStore(props) {
   const router = useRouter();
   const { storeId } = router.query;
-  console.log(router);
+
+  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const handleUpvoteButton = () => {
+    console.log('up vote');
+  };
+
   return (
-    <React.Fragment>
+    <div className={styles.layout}>
       <Head>
-        <title>{storeId}</title>
+        <title>{name}</title>
         <meta name='description' content='Coffee Store' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div>
-        Coffee Store {storeId}
-        <Link href='/'>
-          <a>Back to Home</a>
-        </Link>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href='/'>
+              <a>Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+
+        <div className={`glass ${styles.col2}`}>
+          <div className={styles.iconWrapper}>
+            <Image
+              src='/static/icons/places.svg'
+              width='24'
+              height='24'
+              alt='location icon'
+            />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image
+              src='/static/icons/nearMe.svg'
+              width='24'
+              height='24'
+              alt='location icon'
+            />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image
+              src='/static/icons/star.svg'
+              width='24'
+              height='24'
+              alt='location icon'
+            />
+            <p className={styles.text}>1</p>
+          </div>
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
+        </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 }
 
